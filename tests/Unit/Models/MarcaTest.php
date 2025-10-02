c8d04bb1fc0f36b894654965fff6f48f45ad662e
andredw13L <andreribeiro13l@gmail.com>
2025-10-02 01:10:23 -0300

    Adequando as expectations

--- tests/Unit/Models/MarcaTest.php
+++ tests/Unit/Models/MarcaTest.php
@@ -5,14 +5,20 @@ test('Preencher a Marca', function () {
     $marca->nome = 'Marca Teste';
     $marca->imagem = 'imagem_teste.jpg';
 
-    $this->assertEquals('Marca Teste', $marca->nome);
-    $this->assertEquals('imagem_teste.jpg', $marca->imagem);
+    //$this->assertEquals('Marca Teste', $marca->nome);
+    expect($marca->nome)->toBe('Marca Teste');
+
+    //$this->assertEquals('imagem_teste.jpg', $marca->imagem);
+    expect($marca->imagem)->toBe('imagem_teste.jpg');
 });
 
 test('Verificar os Atributos Fillable', function () {
     $marca = new \App\Models\Marca();
     $fillable = $marca->getFillable();
 
-    $this->assertContains('nome', $fillable);
-    $this->assertContains('imagem', $fillable);
+    //$this->assertContains('nome', $fillable);
+    expect($fillable)->toContain('nome');
+
+    //$this->assertContains('imagem', $fillable);
+    expect($fillable)->toContain('imagem');
 });
