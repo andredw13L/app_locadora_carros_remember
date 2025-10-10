f2662a99bcb4d9b8952e0d9113c064b43a1a8393
andredw13L <andreribeiro13l@gmail.com>
2025-10-10 03:24:46 -0300

    Remover comentário sobre verificação de imagem repetida no método store

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -39,8 +39,6 @@ class MarcaController extends Controller
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens/marcas', 'public');
 
-
-        // TODO: Verificar se a imagem é repetida
         $marca = $this->marca->create([
             'nome' => $request->nome,
             'imagem' => $imagem_urn
