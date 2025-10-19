b255afd42589c07cbd6dde02f71f2c37292597c1
andredw13L <andreribeiro13l@gmail.com>
2025-10-19 01:45:33 -0300

    Repository Design Pattern - Implementando um Abstract Repository

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -2,46 +2,8 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
 
-class MarcaRepository
+class MarcaRepository extends AbstractRepository
 {
-    protected $model;
 
-    public function __construct(Model $model)
-    {
-        return $this->model = $model;
-    }
-
-
-    public function selectAtributosRegistrosRelacionados(string $atributos_modelos_str)
-    {
-        $this->model = $this->model->with($atributos_modelos_str);
-    }
-
-
-    public function filtro(string $filtros)
-    {
-
-        $filtros = explode(';', $filtros);
-
-        foreach ($filtros as $chave => $valor) {
-
-            $condicao = explode(':', $valor);
-
-            $this->model = $this->model->where($condicao[0], $condicao[1], $condicao[2]);
-        }
-    }
-
-
-    public function selectAtributos(array $atributos)
-    {
-        $this->model = $this->model->select($atributos);
-    }
-
-
-    public function getResultado()
-    {
-        return $this->model->get();
-    }
 }
