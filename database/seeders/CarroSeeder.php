d6b23ebba75cbb2c319bbc89a9c488e3905e1342
andredw13L <andreribeiro13l@gmail.com>
2025-11-22 21:25:50 -0300

    chore(carros): cria o seed para o ambiente de desenvolvimento

--- database/seeders/CarroSeeder.php
+++ database/seeders/CarroSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CarroSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class CarroSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('carros')->insert([
+            'modelo_id' => DB::table('modelos')->where('nome', 'Hyundai HB20 1.0')->value('id'),
+            'placa' => 'BRA2E19',
+            'disponivel' => true,
+            'km' => random_int(1 ,2147483647),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
